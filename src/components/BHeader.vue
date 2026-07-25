cc983ed8118016d28ea5bfe2d7f19f738d7fbea9
gxchen <gxchen@student.monash.edu>
2026-07-25 22:16:05 +1000

    add firebase logout nav link

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -37,6 +37,9 @@ const onLogout = () => {
         <li class="nav-item">
           <router-link to="/FireRegister" class="nav-link" active-class="active">Firebase Register</router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/FireLogout" class="nav-link" active-class="active">Firebase Logout</router-link>
+        </li>
       </ul>
     </header>
   </div>
