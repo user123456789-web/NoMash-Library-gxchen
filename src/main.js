4ef5227206e48edd3818ecb43f059b889fa595af
gxchen <gxchen@student.monash.edu>
2026-07-26 10:05:00 +1000

    move firebase config to init.js

--- src/main.js
+++ src/main.js
@@ -11,18 +11,7 @@ import router from './router'
 // import DataTable from 'primevue/datatable'
 // import Column from 'primevue/Column'
 
-import { initializeApp } from 'firebase/app'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBFTGizXznAxNpixRIyDJGx9e4-uUpMchE",
-  authDomain: "week7-gxchen.firebaseapp.com",
-  projectId: "week7-gxchen",
-  storageBucket: "week7-gxchen.firebasestorage.app",
-  messagingSenderId: "500519066233",
-  appId: "1:500519066233:web:50c31625ea072930b152a8"
-}
-
-initializeApp(firebaseConfig)
+import './firebase/init.js'
 
 const app = createApp(App)
 app.use(PrimeVue, { theme: { preset: Aura } })
